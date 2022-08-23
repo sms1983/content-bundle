8c7c3f8acee5be98c6ebc31493a76842657f1b29
Yan Bein <rbeinyan@gmail.com>
2022-08-23 10:37:27 -0300

    Fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('sms-content-bundle');
+        $treeBuilder = new TreeBuilder('cmf_resource');
 
         $treeBuilder->getRootNode()
             ->children()
